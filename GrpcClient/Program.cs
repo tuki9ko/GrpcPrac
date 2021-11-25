0a91b08aab864bb01aaa0d6bd1bfcae4e6204f3b
tuki9ko <arkmisha@gmail.com>
2021-11-25 17:32:16 +0900

    initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using gRpcTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1", 12345, ChannelCredentials.Insecure);
+            var client = new Example.ExampleClient(channel);
+            var machineInfo = client.GetMachineInfo(new Empty());
+            Console.WriteLine($"MachineName: {machineInfo.MachineName} / UserName: {machineInfo.UserName}");
+        }
+    }
+}
---
+++ GrpcServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using gRpcTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { Example.BindService(new ExampleService()) },
+                Ports = { new ServerPort("127.0.0.1", 12345, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("Press any key to shutdown the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    public class ExampleService : Example.ExampleBase
+    {
+        public override Task<ExampleReply> GetMachineInfo(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new ExampleReply
+            {
+                MachineName = Environment.MachineName,
+                UserName = Environment.UserName,
+            });
+        }
+    }
+}
